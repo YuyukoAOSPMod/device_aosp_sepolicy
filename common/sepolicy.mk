a3018bf0aa930ae96576b1b6b121bd988527a1b4
Yuyuko1024 <dingwenxuan4@gmail.com>
2022-09-11 22:03:54 +0800

    sepolicy: Flamingo -> Mod

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -10,20 +10,20 @@ endif
 endif
 
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
-    device/flamingo/sepolicy/common/public
+    device/mod/sepolicy/common/public
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/flamingo/sepolicy/common/private
+    device/mod/sepolicy/common/private
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/flamingo/sepolicy/common/dynamic \
-    device/flamingo/sepolicy/common/system
+    device/mod/sepolicy/common/dynamic \
+    device/mod/sepolicy/common/system
 else
 BOARD_VENDOR_SEPOLICY_DIRS += \
-    device/flamingo/sepolicy/common/dynamic \
-    device/flamingo/sepolicy/common/vendor
+    device/mod/sepolicy/common/dynamic \
+    device/mod/sepolicy/common/vendor
 endif
 
 # Selectively include legacy rules defined by the products
--include device/flamingo/sepolicy/legacy-common/sepolicy.mk
+-include device/mod/sepolicy/legacy-common/sepolicy.mk
